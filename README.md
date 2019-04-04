10ef878f2139d44b907d8ddf0a509e4bfc04561c
samedh <gupta.sam@husky.neu.edu>
2019-04-03 21:09:05 -0400

    started implementing 2-3 tree

---
+++ README.md
@@ -0,0 +1,6 @@
+## Haskell Trees
+
+The goal of this is to learn Haskell by implementing a few different types of trees.
+
+### Currently implementing:
+ - 2-3 Tree
--- twothree.hs
+++ twothree.hs
@@ -6,10 +6,13 @@ module TwoThree
 , toList
 ) where
 
+-- Two Node = data left right
+-- Three Node = data data left middle right
 data TwoThreeTree a = 
     Leaf 
     | TwoNode a TwoThreeTree TwoThreeTree  
     | ThreeNode a a TwoThreeTree TwoThreeTree TwoThreeTree
+    deriving(Eq)
 
 makeTwoThree :: -> TwoThreeTree 
 makeTwoThree = Leaf
@@ -29,4 +32,26 @@ exists (ThreeNode x y left middle right) a
     | otherwise        = exists middle a
 
 
-insert :: 
+-- Tree data Parent -> Tree
+insertHelp :: Ord(a) => (TwoThreeTree a) a (TwoThreeTree a) -> bool 
+
+insertHelp Leaf a _ = TwoNode a Leaf Leaf
+insertHelp (TwoNode x Leaf Leaf) a _
+    | a <= x    = ThreeNode a x Leaf Leaf
+    | otherwise = ThreeNode x a Leaf Leaf
+insertHelp (TwoNode x left right) a _
+    | a <= x    = TwoNode x (insertHelp left a tree) right
+    | otherwise = TwoNode x left (insertHelp right a tree)
+    where tree  = TwoNode x left right
+insertHelp (ThreeNode x y left middle right) a  (TwoNode px pLeft pRight)
+
+-- Tree data -> Tree
+insert :: Ord(a) => (TwoThreeTree a) a -> Bool 
+insert tree a = insertHelp tree a Leaf 
+
+
+
+
+
+
+
